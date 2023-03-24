92f09dfe4e606a4c08fcc9efc6d3d1001a801fd8
YANGLiiN <ielin@qq.com>
2023-03-24 10:16:18 +0800

    [minor] change license to AGPLv3.

--- pulsar-msg-filter-common/src/main/java/io/github/yangl/pulsar/common/MsgFilterUtils.java
+++ pulsar-msg-filter-common/src/main/java/io/github/yangl/pulsar/common/MsgFilterUtils.java
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.github.yangl.pulsar.common;
 
 import static io.github.yangl.pulsar.common.MsgFilterConstants.*;
@@ -10,12 +24,12 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Slf4j
 public class MsgFilterUtils {
-
+    
     public static final boolean filter(String expression, Supplier<Map<String, Object>> supplier) {
         if (StringUtils.isBlank(expression) || StringUtils.equals(Boolean.TRUE.toString(), expression)) {
             return true;
         }
-
+        
         Map<String, Object> messageProperties = supplier.get();
         if (messageProperties.isEmpty()) {
             if (MSGMETADATA_PROPERTIES_NULL_REJECT) {
@@ -24,7 +38,7 @@ public class MsgFilterUtils {
                 return true;
             }
         }
-
+        
         Object rs = null;
         try {
             rs = AV_EVALUATOR.execute(expression, messageProperties);
@@ -35,11 +49,11 @@ public class MsgFilterUtils {
                     new ToStringBuilder(messageProperties),
                     e);
         }
-
+        
         if (rs != null && Boolean.FALSE.equals(rs)) {
             return false;
         }
-
+        
         return true;
     }
 }
